a748783f133c5305104d50b734f453862f5edaaa
kevinkkimutai <kiruikevinkim@gmail.com>
2023-02-07 17:26:22 +0300

    task solved

--- contacts.rb
+++ contacts.rb
@@ -19,6 +19,15 @@ end
 
 def remove_strawberry(contacts)
   # your code here!
+  contacts.each do |person, contact_details|
+    if person == "Freddie Mercury"
+      contact_details.each do |attr, data|
+        if attr == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry" }
+      end
+    end
+  end
+end
 end
 
 # print the output to the terminal for inspection
